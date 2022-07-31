4f3cd788fbdc3e25e1e8930826cab06f05fefbb9
Serge K Lebedev <hello@sergelebe.dev>
2022-08-01 03:48:23 +0600

    Implement viewing and adding bros

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,88 +1,88 @@
-//! This contract implements simple counter backed by storage on blockchain.
-//!
-//! The contract provides methods to [increment] / [decrement] counter and
-//! get it's current value [get_num] or [reset].
+use std::collections::HashMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
+use near_sdk::{near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, env};
+use near_sdk::collections::UnorderedMap;
+use near_sdk::env::is_valid_account_id;
+use near_sdk::serde::{Serialize};
 
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    Cryptobros,
+    Cryptobro { account_id: AccountId },
+}
 
-#[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Counter {
-    val: i8,
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum BrosStatus {
+    Following,
+    FollowedBy,
+    Bros,
 }
 
 #[near_bindgen]
-impl Counter {
-    /// Public method: Returns the counter value.
-    pub fn get_num(&self) -> i8 {
-        return self.val;
-    }
-
-    /// Public method: Increment the counter.
-    pub fn increment(&mut self) {
-        self.val += 1;
-        log!("Increased number to {}", self.val);
-    }
-
-    /// Public method: Decrement the counter.
-    pub fn decrement(&mut self) {
-        self.val -= 1;
-        log!("Decreased number to {}", self.val);
-    }
-
-    /// Public method - Reset to zero.
-    pub fn reset(&mut self) {
-        self.val = 0;
-        log!("Reset counter to zero");
-    }
+#[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
+pub struct Cryptobros {
+    bros: UnorderedMap<AccountId, UnorderedMap<AccountId, BrosStatus>>,
 }
 
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be: `cargo test`
- * Note: 'rust-counter-tutorial' comes from cargo.toml's 'name' key
- */
-
-// use the attribute below for unit tests
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn increment() {
-        // instantiate a contract variable with the counter at zero
-        let mut contract = Counter { val: 0 };
-        contract.increment();
-        assert_eq!(1, contract.get_num());
+#[near_bindgen]
+impl Cryptobros {
+    #[init]
+    pub fn new() -> Self {
+        Self {
+            bros: UnorderedMap::new(StorageKey::Cryptobros),
+        }
     }
 
-    #[test]
-    fn decrement() {
-        let mut contract = Counter { val: 0 };
-        contract.decrement();
-        assert_eq!(-1, contract.get_num());
+    pub fn get_bros(&self, account_id: String) -> HashMap<AccountId, BrosStatus> {
+        assert!(is_valid_account_id(account_id.as_bytes()), "Specify correct account ID");
+
+        let connections = self.bros.get(&AccountId::new_unchecked(account_id)).unwrap();
+
+        let keys = connections.keys_as_vector();
+        keys.iter().fold(HashMap::new(), |mut hashmap, key| {
+            hashmap.insert(key.clone(), connections.get(&key).unwrap());
+            hashmap
+        })
     }
 
-    #[test]
-    fn increment_and_reset() {
-        let mut contract = Counter { val: 0 };
-        contract.increment();
-        contract.reset();
-        assert_eq!(0, contract.get_num());
+    fn get_or_create_connections(&mut self, account_id: &AccountId) -> UnorderedMap<AccountId, BrosStatus> {
+        self.bros.get(&account_id).unwrap_or_else(|| {
+            let new_unordered_map = UnorderedMap::new(StorageKey::Cryptobro { account_id: account_id.clone() });
+
+            self.bros.insert(&account_id, &new_unordered_map);
+
+            new_unordered_map
+        })
     }
 
-    #[test]
-    #[should_panic]
-    fn panics_on_overflow() {
-        let mut contract = Counter { val: 127 };
-        contract.increment();
-    }
+    pub fn add_bro(&mut self, account_id: String) {
+        assert!(is_valid_account_id(account_id.as_bytes()), "Specify correct account ID");
 
-    #[test]
-    #[should_panic]
-    fn panics_on_underflow() {
-        let mut contract = Counter { val: -128 };
-        contract.decrement();
+        let caller_account_id = env::predecessor_account_id();
+        let target_account_id: AccountId = AccountId::new_unchecked(account_id.clone());
+
+        assert!(caller_account_id != target_account_id, "No need to add yourself as a bro, bro");
+
+        let mut caller_connections = self.get_or_create_connections(&caller_account_id);
+        let mut target_connections = self.get_or_create_connections(&target_account_id);
+
+        let caller_target_connection = caller_connections.get(&target_account_id);
+        let new_statuses: Option<(BrosStatus, BrosStatus)> = match caller_target_connection {
+            None => Some((BrosStatus::Following, BrosStatus::FollowedBy)),
+            Some(BrosStatus::FollowedBy) => Some((BrosStatus::Bros, BrosStatus::Bros)),
+            _ => None,
+        };
+
+        match new_statuses {
+            Some((caller_target_status, target_caller_status)) => {
+                caller_connections.insert(&target_account_id, &caller_target_status);
+                self.bros.insert(&caller_account_id, &caller_connections);
+
+                target_connections.insert(&caller_account_id, &target_caller_status);
+                self.bros.insert(&target_account_id, &target_connections);
+            }
+            _ => {}
+        }
     }
 }
